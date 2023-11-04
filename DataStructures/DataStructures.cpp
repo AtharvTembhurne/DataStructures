6581e6ba59bcc633509e014f98015eb55523fa30
atharv tembhurne <atharvtembhurne10@gmail.com>
2023-11-04 11:50:16 +0530

    Initial Commit

---
+++ DataStructures/DataStructures.cpp
@@ -0,0 +1,12 @@
+﻿// DataStructures.cpp : Defines the entry point for the application.
+//
+
+#include "DataStructures.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Data Structures by Atharva Tembhurne" << endl;
+	return 0;
+}
